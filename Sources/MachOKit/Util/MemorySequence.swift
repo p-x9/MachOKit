f26cab71995a767b12c4f9e75e3156a29b1b0c4e
p-x9 <50244599+p-x9@users.noreply.github.com>
2023-11-29 15:49:34 +0900

    fix offset calculation in MemorySequence

--- Sources/MachOKit/Util/MemorySequence.swift
+++ Sources/MachOKit/Util/MemorySequence.swift
@@ -12,23 +12,19 @@ public struct MemorySequence<T>: Sequence {
     public typealias Element = T
 
     private let basePointer: UnsafePointer<T>
-    private let stride: Int
     private let numberOfElements: Int
 
     init(
         basePointer: UnsafePointer<T>,
-        stride: Int,
         numberOfElements: Int
     ) {
         self.basePointer = basePointer
-        self.stride = stride
         self.numberOfElements = numberOfElements
     }
 
     public func makeIterator() -> Iterator {
         Iterator(
             basePointer: basePointer,
-            stride: stride,
             numberOfElements: numberOfElements
         )
     }
@@ -39,18 +35,15 @@ extension MemorySequence {
         public typealias Element = T
 
         private let basePointer: UnsafePointer<T>
-        private let stride: Int
         private let numberOfElements: Int
 
         private var nextIndex: Int = 0
 
         init(
             basePointer: UnsafePointer<T>,
-            stride: Int,
             numberOfElements: Int
         ) {
             self.basePointer = basePointer
-            self.stride = stride
             self.numberOfElements = numberOfElements
         }
 
@@ -58,7 +51,7 @@ extension MemorySequence {
             guard nextIndex < numberOfElements else { return nil }
             defer { nextIndex += 1 }
             return basePointer
-                .advanced(by: nextIndex * stride)
+                .advanced(by: nextIndex)
                 .pointee
         }
     }
