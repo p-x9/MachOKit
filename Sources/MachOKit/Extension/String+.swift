2808b0a13c76185cbffe97b67fa05e98898ff62a
p-x9 <50244599+p-x9@users.noreply.github.com>
2023-12-01 14:17:16 +0900

    fix String initializer for tuple of CChar

--- Sources/MachOKit/Extension/String+.swift
+++ Sources/MachOKit/Extension/String+.swift
@@ -12,10 +12,13 @@ extension String {
     typealias CCharTuple16 = (CChar, CChar, CChar, CChar, CChar, CChar, CChar, CChar, CChar, CChar, CChar, CChar, CChar, CChar, CChar, CChar)
 
     init(tuple: CCharTuple16) {
-        self.init(
-            cString: Mirror(reflecting: tuple).children.compactMap {
+        self = Mirror(reflecting: tuple).children
+            .compactMap {
                 $0.value as? CChar
             }
-        )
+            .map(UInt8.init)
+            .map(UnicodeScalar.init)
+            .map(String.init)
+            .joined()
     }
 }
