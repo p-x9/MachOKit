476f442358e434b032ba723b54cce20d1e3aab27
p-x9 <50244599+p-x9@users.noreply.github.com>
2023-12-05 20:00:42 +0900

    implemented function to get tools list in `BuildVersionCommand` for MachOFile

--- Sources/MachOKit/LoadCommand/BuildVersionCommand.swift
+++ Sources/MachOKit/LoadCommand/BuildVersionCommand.swift
@@ -33,7 +33,9 @@ extension BuildVersionCommand {
     public var sdk: Version {
         .init(layout.sdk)
     }
+}
 
+extension BuildVersionCommand {
     public func tools(
         cmdsStart: UnsafeRawPointer
     ) -> MemorySequence<BuildToolVersion> {
@@ -43,6 +45,31 @@ extension BuildVersionCommand {
             .assumingMemoryBound(to: BuildToolVersion.self)
         return .init(
             basePointer: base,
+            numberOfElements: Int(layout.ntools)
+        )
+    }
+}
+
+extension BuildVersionCommand {
+    public func tools(
+        in machO: MachOFile
+    ) -> DataSequence<BuildToolVersion> {
+        let offset = machO.cmdsStartOffset + offset + MemoryLayout<Layout>.size
+        machO.fileHandle.seek(toFileOffset: UInt64(offset))
+        let data = machO.fileHandle.readData(
+            ofLength: Int(layout.ntools) * MemoryLayout<BuildToolVersion>.size
+        )
+        if machO.isSwapped {
+            data.withUnsafeBytes {
+                guard let baseAddress = $0.baseAddress else { return }
+                let ptr = UnsafeMutableRawPointer(mutating: baseAddress)
+                    .assumingMemoryBound(to: build_tool_version.self)
+                swap_build_tool_version(ptr, layout.ntools, NXHostByteOrder())
+            }
+        }
+
+        return .init(
+            data: data,
             numberOfElements: Int(layout.ntools)
         )
     }
