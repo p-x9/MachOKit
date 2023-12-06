43057c5cf16d9bdcd30be9f5d7bf232dd2bdbf32
p-x9 <50244599+p-x9@users.noreply.github.com>
2023-12-06 17:25:19 +0900

    refactor file structure

--- Sources/MachOKit/Util/Sequence/DataSequence.swift
+++ Sources/MachOKit/Util/Sequence/DataSequence.swift
@@ -1,61 +1,12 @@
 //
-//  MemorySequence.swift
-//  
+//  DataSequence.swift
 //
-//  Created by p-x9 on 2023/11/29.
+//
+//  Created by p-x9 on 2023/12/06.
 //  
 //
 
 import Foundation
-
-public struct MemorySequence<T>: Sequence {
-    public typealias Element = T
-
-    private let basePointer: UnsafePointer<T>
-    private let numberOfElements: Int
-
-    init(
-        basePointer: UnsafePointer<T>,
-        numberOfElements: Int
-    ) {
-        self.basePointer = basePointer
-        self.numberOfElements = numberOfElements
-    }
-
-    public func makeIterator() -> Iterator {
-        Iterator(
-            basePointer: basePointer,
-            numberOfElements: numberOfElements
-        )
-    }
-}
-
-extension MemorySequence {
-    public struct Iterator: IteratorProtocol {
-        public typealias Element = T
-
-        private let basePointer: UnsafePointer<T>
-        private let numberOfElements: Int
-
-        private var nextIndex: Int = 0
-
-        init(
-            basePointer: UnsafePointer<T>,
-            numberOfElements: Int
-        ) {
-            self.basePointer = basePointer
-            self.numberOfElements = numberOfElements
-        }
-
-        public mutating func next() -> Element? {
-            guard nextIndex < numberOfElements else { return nil }
-            defer { nextIndex += 1 }
-            return basePointer
-                .advanced(by: nextIndex)
-                .pointee
-        }
-    }
-}
 
 public struct DataSequence<T>: Sequence {
     public typealias Element = T
