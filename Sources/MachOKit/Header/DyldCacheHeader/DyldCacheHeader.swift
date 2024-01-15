ff7d064128b5297890fefe2dbf5fec449456bfae
p-x9 <50244599+p-x9@users.noreply.github.com>
2024-01-15 21:19:42 +0900

    Identify CPU architecture using header magic

--- Sources/MachOKit/Header/DyldCacheHeader/DyldCacheHeader.swift
+++ Sources/MachOKit/Header/DyldCacheHeader/DyldCacheHeader.swift
@@ -56,3 +56,43 @@ extension DyldCacheHeader {
         .init(uuid: layout.symbolFileUUID)
     }
 }
+
+extension DyldCacheHeader {
+    // https://github.com/apple-oss-distributions/dyld/blob/d1a0f6869ece370913a3f749617e457f3b4cd7c4/dyld/SharedCacheRuntime.cpp#L100
+    // https://github.com/opensource-apple/dyld/blob/3f928f32597888c5eac6003b9199d972d49857b5/src/dyld.cpp#L3112
+    internal var _cpuType: CPUType? {
+        switch magic {
+        case "dyld_v1    i386": return .i386
+        case "dyld_v1  x86_64": return .x86_64
+        case "dyld_v1 x86_64h": return .x86_64
+        case "dyld_v1   armv5": return .arm
+        case "dyld_v1   armv6": return .arm
+        case "dyld_v1  armv7f": return .arm
+        case "dyld_v1  armv7k": return .arm
+        case "dyld_v1   armv7": return .arm
+        case "dyld_v1  armv7s": return .arm
+        case "dyld_v1  arm64e": return .arm64
+        case "dyld_v1   arm64": return .arm64
+        case "dyld_v1arm64_32": return .arm64_32
+        default: return nil
+        }
+    }
+
+    internal var _cpuSubType: CPUSubType? {
+        switch magic {
+        case "dyld_v1    i386": return .i386(.i386_all)
+        case "dyld_v1  x86_64": return .x86(.x86_64_all)
+        case "dyld_v1 x86_64h": return .x86(.x86_64_h)
+        case "dyld_v1   armv5": return .arm(.arm_v5tej) // == CPU_SUBTYPE_ARM_V5
+        case "dyld_v1   armv6": return .arm(.arm_v6)
+        case "dyld_v1  armv7f": return .arm(.arm_v7f)
+        case "dyld_v1  armv7k": return .arm(.arm_v7k)
+        case "dyld_v1   armv7": return .arm(.arm_v7)
+        case "dyld_v1  armv7s": return .arm(.arm_v7s)
+        case "dyld_v1  arm64e": return .arm64(.arm64_all)
+        case "dyld_v1   arm64": return .arm64(.arm64_all)
+        case "dyld_v1arm64_32": return .arm64_32(.arm64_32_all)
+        default: return nil
+        }
+    }
+}
