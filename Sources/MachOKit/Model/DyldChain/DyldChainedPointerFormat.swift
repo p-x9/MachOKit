9f2a20b20182ff40ac0856803a1a183a0b8f01ed
p-x9 <50244599+p-x9@users.noreply.github.com>
2024-02-17 16:38:42 +0900

    fix raw value of `DyldChainedPointerFormat`

--- Sources/MachOKit/Model/DyldChain/DyldChainedPointerFormat.swift
+++ Sources/MachOKit/Model/DyldChain/DyldChainedPointerFormat.swift
@@ -8,9 +8,9 @@
 
 import Foundation
 
-public enum DyldChainedPointerFormat: UInt16 {
+public enum DyldChainedPointerFormat {
     /// DYLD_CHAINED_PTR_ARM64E
-    case arm64e = 1
+    case arm64e
     /// DYLD_CHAINED_PTR_64
     case _64
     /// DYLD_CHAINED_PTR_32
@@ -21,8 +21,8 @@ public enum DyldChainedPointerFormat: UInt16 {
     case _32_firmware
     /// DYLD_CHAINED_PTR_64_OFFSET
     case _64_offset
-    /// DYLD_CHAINED_PTR_ARM64E_OFFSET
-    case arm64e_offset
+//    /// DYLD_CHAINED_PTR_ARM64E_OFFSET
+//    case arm64e_offset
     /// DYLD_CHAINED_PTR_ARM64E_KERNEL
     case arm64e_kernel
     /// DYLD_CHAINED_PTR_64_KERNEL_CACHE
@@ -46,13 +46,53 @@ extension DyldChainedPointerFormat: CustomStringConvertible {
         case ._32_cache: "DYLD_CHAINED_PTR_32_CACHE"
         case ._32_firmware: "DYLD_CHAINED_PTR_32_FIRMWARE"
         case ._64_offset: "DYLD_CHAINED_PTR_64_OFFSET"
-        case .arm64e_offset: "DYLD_CHAINED_PTR_ARM64E_OFFSET"
+//        case .arm64e_offset: "DYLD_CHAINED_PTR_ARM64E_OFFSET"
         case .arm64e_kernel: "DYLD_CHAINED_PTR_ARM64E_KERNEL"
         case ._64_kernel_cache: "DYLD_CHAINED_PTR_64_KERNEL_CACHE"
         case .arm64e_userland: "DYLD_CHAINED_PTR_ARM64E_USERLAND"
         case .arm64e_firmware: "DYLD_CHAINED_PTR_ARM64E_FIRMWARE"
         case .x86_64_kernel_cache: "DYLD_CHAINED_PTR_X86_64_KERNEL_CACHE"
         case .arm64e_userland24: "DYLD_CHAINED_PTR_ARM64E_USERLAND24"
+        }
+    }
+}
+
+extension DyldChainedPointerFormat: RawRepresentable {
+    public typealias RawValue = UInt16
+
+    public init?(rawValue: RawValue) {
+        switch rawValue {
+        case UInt16(DYLD_CHAINED_PTR_ARM64E): self = .arm64e
+        case UInt16(DYLD_CHAINED_PTR_64): self = ._64
+        case UInt16(DYLD_CHAINED_PTR_32): self = ._32
+        case UInt16(DYLD_CHAINED_PTR_32_CACHE): self = ._32_cache
+        case UInt16(DYLD_CHAINED_PTR_32_FIRMWARE): self = ._32_firmware
+        case UInt16(DYLD_CHAINED_PTR_64_OFFSET): self = ._64_offset
+//        case UInt16(DYLD_CHAINED_PTR_ARM64E_OFFSET): self = .arm64e_offset
+        case UInt16(DYLD_CHAINED_PTR_ARM64E_KERNEL): self = .arm64e_kernel
+        case UInt16(DYLD_CHAINED_PTR_64_KERNEL_CACHE): self = ._64_kernel_cache
+        case UInt16(DYLD_CHAINED_PTR_ARM64E_USERLAND): self = .arm64e_userland
+        case UInt16(DYLD_CHAINED_PTR_ARM64E_FIRMWARE): self = .arm64e_firmware
+        case UInt16(DYLD_CHAINED_PTR_X86_64_KERNEL_CACHE): self = .x86_64_kernel_cache
+        case UInt16(DYLD_CHAINED_PTR_ARM64E_USERLAND24): self = .arm64e_userland24
+        default: return nil
+        }
+    }
+    public var rawValue: RawValue {
+        switch self {
+        case .arm64e: UInt16(DYLD_CHAINED_PTR_ARM64E)
+        case ._64: UInt16(DYLD_CHAINED_PTR_64)
+        case ._32: UInt16(DYLD_CHAINED_PTR_32)
+        case ._32_cache: UInt16(DYLD_CHAINED_PTR_32_CACHE)
+        case ._32_firmware: UInt16(DYLD_CHAINED_PTR_32_FIRMWARE)
+        case ._64_offset: UInt16(DYLD_CHAINED_PTR_64_OFFSET)
+//        case .arm64e_offset: UInt16(DYLD_CHAINED_PTR_ARM64E_OFFSET)
+        case .arm64e_kernel: UInt16(DYLD_CHAINED_PTR_ARM64E_KERNEL)
+        case ._64_kernel_cache: UInt16(DYLD_CHAINED_PTR_64_KERNEL_CACHE)
+        case .arm64e_userland: UInt16(DYLD_CHAINED_PTR_ARM64E_USERLAND)
+        case .arm64e_firmware: UInt16(DYLD_CHAINED_PTR_ARM64E_FIRMWARE)
+        case .x86_64_kernel_cache: UInt16(DYLD_CHAINED_PTR_X86_64_KERNEL_CACHE)
+        case .arm64e_userland24: UInt16(DYLD_CHAINED_PTR_ARM64E_USERLAND24)
         }
     }
 }
