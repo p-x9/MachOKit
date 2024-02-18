70c38e3eb31d112237165c51b38f078050d7c54f
p-x9 <50244599+p-x9@users.noreply.github.com>
2024-02-18 16:37:59 +0900

    delete old `DyldChainedPage`

--- Sources/MachOKit/Model/DyldChain/DyldChainedSymbolsFormat.swift
+++ Sources/MachOKit/Model/DyldChain/DyldChainedSymbolsFormat.swift
@@ -12,16 +12,3 @@ public enum DyldChainedSymbolsFormat: UInt32 {
     case uncompressed
     case zlibCompressed
 }
-
-public struct DyldChainedPage {
-    public let offset: UInt16
-    public let index: Int
-
-    public var isNone: Bool {
-        offset == DYLD_CHAINED_PTR_START_NONE
-    }
-
-    public var isMulti: Bool {
-        offset & UInt16(DYLD_CHAINED_PTR_START_MULTI) > 0
-    }
-}
