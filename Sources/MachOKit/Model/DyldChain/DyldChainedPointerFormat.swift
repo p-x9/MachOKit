2455a1989bfec9d828c657ae068d135fd55bdd05
p-x9 <50244599+p-x9@users.noreply.github.com>
2024-02-18 16:30:15 +0900

    add stride info to pointer format

--- Sources/MachOKit/Model/DyldChain/DyldChainedPointerFormat.swift
+++ Sources/MachOKit/Model/DyldChain/DyldChainedPointerFormat.swift
@@ -3,7 +3,7 @@
 //
 //
 //  Created by p-x9 on 2024/01/11.
-//  
+//
 //
 
 import Foundation
@@ -21,8 +21,8 @@ public enum DyldChainedPointerFormat {
     case _32_firmware
     /// DYLD_CHAINED_PTR_64_OFFSET
     case _64_offset
-//    /// DYLD_CHAINED_PTR_ARM64E_OFFSET
-//    case arm64e_offset
+    //    /// DYLD_CHAINED_PTR_ARM64E_OFFSET
+    //    case arm64e_offset
     /// DYLD_CHAINED_PTR_ARM64E_KERNEL
     case arm64e_kernel
     /// DYLD_CHAINED_PTR_64_KERNEL_CACHE
@@ -46,7 +46,7 @@ extension DyldChainedPointerFormat: CustomStringConvertible {
         case ._32_cache: "DYLD_CHAINED_PTR_32_CACHE"
         case ._32_firmware: "DYLD_CHAINED_PTR_32_FIRMWARE"
         case ._64_offset: "DYLD_CHAINED_PTR_64_OFFSET"
-//        case .arm64e_offset: "DYLD_CHAINED_PTR_ARM64E_OFFSET"
+            //        case .arm64e_offset: "DYLD_CHAINED_PTR_ARM64E_OFFSET"
         case .arm64e_kernel: "DYLD_CHAINED_PTR_ARM64E_KERNEL"
         case ._64_kernel_cache: "DYLD_CHAINED_PTR_64_KERNEL_CACHE"
         case .arm64e_userland: "DYLD_CHAINED_PTR_ARM64E_USERLAND"
@@ -68,7 +68,7 @@ extension DyldChainedPointerFormat: RawRepresentable {
         case UInt16(DYLD_CHAINED_PTR_32_CACHE): self = ._32_cache
         case UInt16(DYLD_CHAINED_PTR_32_FIRMWARE): self = ._32_firmware
         case UInt16(DYLD_CHAINED_PTR_64_OFFSET): self = ._64_offset
-//        case UInt16(DYLD_CHAINED_PTR_ARM64E_OFFSET): self = .arm64e_offset
+            //        case UInt16(DYLD_CHAINED_PTR_ARM64E_OFFSET): self = .arm64e_offset
         case UInt16(DYLD_CHAINED_PTR_ARM64E_KERNEL): self = .arm64e_kernel
         case UInt16(DYLD_CHAINED_PTR_64_KERNEL_CACHE): self = ._64_kernel_cache
         case UInt16(DYLD_CHAINED_PTR_ARM64E_USERLAND): self = .arm64e_userland
@@ -78,6 +78,7 @@ extension DyldChainedPointerFormat: RawRepresentable {
         default: return nil
         }
     }
+
     public var rawValue: RawValue {
         switch self {
         case .arm64e: UInt16(DYLD_CHAINED_PTR_ARM64E)
@@ -86,13 +87,44 @@ extension DyldChainedPointerFormat: RawRepresentable {
         case ._32_cache: UInt16(DYLD_CHAINED_PTR_32_CACHE)
         case ._32_firmware: UInt16(DYLD_CHAINED_PTR_32_FIRMWARE)
         case ._64_offset: UInt16(DYLD_CHAINED_PTR_64_OFFSET)
-//        case .arm64e_offset: UInt16(DYLD_CHAINED_PTR_ARM64E_OFFSET)
+            //        case .arm64e_offset: UInt16(DYLD_CHAINED_PTR_ARM64E_OFFSET)
         case .arm64e_kernel: UInt16(DYLD_CHAINED_PTR_ARM64E_KERNEL)
         case ._64_kernel_cache: UInt16(DYLD_CHAINED_PTR_64_KERNEL_CACHE)
         case .arm64e_userland: UInt16(DYLD_CHAINED_PTR_ARM64E_USERLAND)
         case .arm64e_firmware: UInt16(DYLD_CHAINED_PTR_ARM64E_FIRMWARE)
         case .x86_64_kernel_cache: UInt16(DYLD_CHAINED_PTR_X86_64_KERNEL_CACHE)
         case .arm64e_userland24: UInt16(DYLD_CHAINED_PTR_ARM64E_USERLAND24)
+        }
+    }
+}
+
+extension DyldChainedPointerFormat {
+    // byte
+    var is64Bit: Bool {
+        switch self {
+        case .arm64e, .arm64e_userland, .arm64e_userland24,
+                .arm64e_kernel, .arm64e_firmware,
+                ._64, ._64_offset, ._64_kernel_cache,
+                .x86_64_kernel_cache:
+            return true
+        case ._32, ._32_firmware, ._32_cache:
+            return false
+        }
+    }
+}
+
+extension DyldChainedPointerFormat {
+    // https://github.com/apple-oss-distributions/dyld/blob/d1a0f6869ece370913a3f749617e457f3b4cd7c4/common/MachOLayout.cpp#L2145
+    var stride: Int {
+        switch self {
+        case .arm64e, .arm64e_userland, .arm64e_userland24:
+            return 8
+        case .arm64e_kernel, .arm64e_firmware,
+                ._32, ._32_firmware, ._32_cache,
+                ._64, ._64_offset, ._64_kernel_cache:
+            return 4
+        case .x86_64_kernel_cache:
+            return 1
         }
     }
 }
