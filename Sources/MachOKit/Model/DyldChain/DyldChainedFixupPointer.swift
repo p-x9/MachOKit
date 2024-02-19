8284104eb7c3270244a19f71a131d4da3c0deb6b
p-x9 <50244599+p-x9@users.noreply.github.com>
2024-02-19 16:34:17 +0900

    add `DyldChainedFixupPointer`

---
+++ Sources/MachOKit/Model/DyldChain/DyldChainedFixupPointer.swift
@@ -0,0 +1,14 @@
+//
+//  DyldChainedFixupPointer.swift
+//
+//
+//  Created by p-x9 on 2024/02/19.
+//  
+//
+
+import Foundation
+
+public struct DyldChainedFixupPointer {
+    public let offset: Int
+    public let fixupInfo: DyldChainedFixupPointerInfo
+}
