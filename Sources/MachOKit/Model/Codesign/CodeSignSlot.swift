a3f813c2cdb2eb6a738798cdaa4f5947d6153ec6
p-x9 <50244599+p-x9@users.noreply.github.com>
2024-03-03 16:47:02 +0900

    fix rawValue of CodeSignSlot

--- Sources/MachOKit/Model/Codesign/CodeSignSlot.swift
+++ Sources/MachOKit/Model/Codesign/CodeSignSlot.swift
@@ -9,7 +9,7 @@
 import Foundation
 import MachOKitC
 
-public enum CodeSignSlot: UInt32 {
+public enum CodeSignSlot {
     /// CSSLOT_CODEDIRECTORY
     case codedirectory
     /// CSSLOT_INFOSLOT
@@ -46,6 +46,54 @@ public enum CodeSignSlot: UInt32 {
     case ticketslot
 }
 
+extension CodeSignSlot: RawRepresentable {
+    public typealias RawValue = UInt32
+
+    public init?(rawValue: RawValue) {
+        switch rawValue {
+        case RawValue(CSSLOT_CODEDIRECTORY): self = .codedirectory
+        case RawValue(CSSLOT_INFOSLOT): self = .infoslot
+        case RawValue(CSSLOT_REQUIREMENTS): self = .requirements
+        case RawValue(CSSLOT_RESOURCEDIR): self = .resourcedir
+        case RawValue(CSSLOT_APPLICATION): self = .application
+        case RawValue(CSSLOT_ENTITLEMENTS): self = .entitlements
+        case RawValue(CSSLOT_DER_ENTITLEMENTS): self = .der_entitlements
+        case RawValue(CSSLOT_LAUNCH_CONSTRAINT_SELF): self = .launch_constraint_self
+        case RawValue(CSSLOT_LAUNCH_CONSTRAINT_PARENT): self = .launch_constraint_parent
+        case RawValue(CSSLOT_LAUNCH_CONSTRAINT_RESPONSIBLE): self = .launch_constraint_responsible
+        case RawValue(CSSLOT_LIBRARY_CONSTRAINT): self = .library_constraint
+        case RawValue(CSSLOT_ALTERNATE_CODEDIRECTORIES): self = .alternate_codedirectories
+        case RawValue(CSSLOT_ALTERNATE_CODEDIRECTORY_MAX): self = .alternate_codedirectory_max
+        case RawValue(CSSLOT_ALTERNATE_CODEDIRECTORY_LIMIT): self = .alternate_codedirectory_limit
+        case RawValue(CSSLOT_SIGNATURESLOT): self = .signatureslot
+        case RawValue(CSSLOT_IDENTIFICATIONSLOT): self = .identificationslot
+        case RawValue(CSSLOT_TICKETSLOT): self = .ticketslot
+        default: return nil
+        }
+    }
+    public var rawValue: RawValue {
+        switch self {
+        case .codedirectory: RawValue(CSSLOT_CODEDIRECTORY)
+        case .infoslot: RawValue(CSSLOT_INFOSLOT)
+        case .requirements: RawValue(CSSLOT_REQUIREMENTS)
+        case .resourcedir: RawValue(CSSLOT_RESOURCEDIR)
+        case .application: RawValue(CSSLOT_APPLICATION)
+        case .entitlements: RawValue(CSSLOT_ENTITLEMENTS)
+        case .der_entitlements: RawValue(CSSLOT_DER_ENTITLEMENTS)
+        case .launch_constraint_self: RawValue(CSSLOT_LAUNCH_CONSTRAINT_SELF)
+        case .launch_constraint_parent: RawValue(CSSLOT_LAUNCH_CONSTRAINT_PARENT)
+        case .launch_constraint_responsible: RawValue(CSSLOT_LAUNCH_CONSTRAINT_RESPONSIBLE)
+        case .library_constraint: RawValue(CSSLOT_LIBRARY_CONSTRAINT)
+        case .alternate_codedirectories: RawValue(CSSLOT_ALTERNATE_CODEDIRECTORIES)
+        case .alternate_codedirectory_max: RawValue(CSSLOT_ALTERNATE_CODEDIRECTORY_MAX)
+        case .alternate_codedirectory_limit: RawValue(CSSLOT_ALTERNATE_CODEDIRECTORY_LIMIT)
+        case .signatureslot: RawValue(CSSLOT_SIGNATURESLOT)
+        case .identificationslot: RawValue(CSSLOT_IDENTIFICATIONSLOT)
+        case .ticketslot: RawValue(CSSLOT_TICKETSLOT)
+        }
+    }
+}
+
 extension CodeSignSlot: CustomStringConvertible {
     public var description: String {
         switch self {
@@ -68,4 +116,4 @@ extension CodeSignSlot: CustomStringConvertible {
         case .ticketslot: "CSSLOT_TICKETSLOT"
         }
     }
-}
+    }
