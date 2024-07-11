3b17cad79a12c980429e4a0280f6fd824025c676
p-x9 <50244599+p-x9@users.noreply.github.com>
2024-07-11 20:07:35 +0900

    Support loader ref

--- Sources/MachOKit/Model/DyldCache/Loader/PrebuiltLoader.swift
+++ Sources/MachOKit/Model/DyldCache/Loader/PrebuiltLoader.swift
@@ -16,10 +16,32 @@ public struct PrebuiltLoader: LayoutWrapper {
 }
 
 extension PrebuiltLoader {
+    // always true
+    public var isPrebuilt: Bool {
+        layout.isPrebuilt != 0
+    }
+
+    public var ref: LoaderRef {
+        .init(layout: layout.ref)
+    }
+
     public func path(in cache: DyldCache) -> String? {
         guard let offset = cache.fileOffset(
             of: numericCast(address) + numericCast(layout.pathOffset)
         ) else { return nil }
         return cache.fileHandle.readString(offset: offset)
+    }
+
+    public func dependentLoaderRefs(in cache: DyldCache) -> DataSequence<LoaderRef>? {
+        guard layout.dependentLoaderRefsArrayOffset != 0,
+              let offset = cache.fileOffset(
+                of: numericCast(address) + numericCast(layout.dependentLoaderRefsArrayOffset)
+              ) else {
+            return nil
+        }
+        return cache.fileHandle.readDataSequence(
+            offset: offset,
+            numberOfElements: numericCast(layout.depCount)
+        )
     }
 }
