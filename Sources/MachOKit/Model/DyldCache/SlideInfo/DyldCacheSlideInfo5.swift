c5f366578afefa84edf61b56cc60f256a4a99154
p-x9 <50244599+p-x9@users.noreply.github.com>
2024-07-25 12:30:59 +0900

    Implemented `DyldCacheSlideInfo5`

---
+++ Sources/MachOKit/Model/DyldCache/SlideInfo/DyldCacheSlideInfo5.swift
@@ -0,0 +1,49 @@
+//
+//  DyldCacheSlideInfo5.swift
+//
+//
+//  Created by p-x9 on 2024/07/25
+//  
+//
+
+import Foundation
+import MachOKitC
+
+public struct DyldCacheSlideInfo5: LayoutWrapper {
+    public typealias Layout = dyld_cache_slide_info5
+
+    public var layout: Layout
+    public var offset: Int
+}
+
+// MARK: - PageStart
+extension DyldCacheSlideInfo5 {
+    public struct PageStart {
+        public let value: UInt16
+
+        public var isNoRebase: Bool {
+            value == DYLD_CACHE_SLIDE_V5_PAGE_ATTR_NO_REBASE
+        }
+    }
+}
+
+// MARK: - function & proerty
+extension DyldCacheSlideInfo5 {
+    public var pageSize: Int {
+        numericCast(layout.page_size)
+    }
+}
+
+extension DyldCacheSlideInfo5 {
+    public var numberOfPageStarts: Int {
+        numericCast(layout.page_starts_count)
+    }
+
+    public func pageStarts(in cache: DyldCache) -> DataSequence<PageStart>? {
+        let pageStartsOffset = layoutSize
+        return cache.fileHandle.readDataSequence(
+            offset: numericCast(offset) + numericCast(pageStartsOffset),
+            numberOfElements: numberOfPageStarts
+        )
+    }
+}
