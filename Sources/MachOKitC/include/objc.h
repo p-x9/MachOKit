3d4c95b37fcdb5fbc3147d6fb3426701f4ce6d42
p-x9 <50244599+p-x9@users.noreply.github.com>
2024-10-05 04:59:33 +0900

    Add structs for objc header optimization rw

--- Sources/MachOKitC/include/objc.h
+++ Sources/MachOKitC/include/objc.h
@@ -25,4 +25,30 @@ struct objc_optimization
     uint64_t relativeMethodSelectorBaseAddressOffset;
 };
 
+// https://github.com/apple-oss-distributions/dyld/blob/a571176e8e00c47e95b95e3156820ebec0cbd5e6/common/OptimizerObjC.h#L656
+struct header_info_rw_64 {
+    uint64_t isLoaded              : 1;
+    uint64_t allClassesRealized    : 1;
+    uint64_t next                  : 62;
+};
+
+struct header_info_rw_32 {
+    uint32_t isLoaded              : 1;
+    uint32_t allClassesRealized    : 1;
+    uint32_t next                  : 30;
+};
+
+// https://github.com/apple-oss-distributions/dyld/blob/a571176e8e00c47e95b95e3156820ebec0cbd5e6/common/OptimizerObjC.h#L818
+struct objc_headeropt_rw_t_64 {
+    uint32_t count;
+    uint32_t entsize;
+    struct header_info_rw_64 headers[0];  // sorted by mhdr address
+};
+
+struct objc_headeropt_rw_t_32 {
+    uint32_t count;
+    uint32_t entsize;
+    struct header_info_rw_32 headers[0];  // sorted by mhdr address
+};
+
 #endif /* objc_h */
