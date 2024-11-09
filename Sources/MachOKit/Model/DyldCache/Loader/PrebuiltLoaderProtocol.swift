c58aa5f78d244857b682bb0a1e44cc1100fcb13f
p-x9 <50244599+p-x9@users.noreply.github.com>
2024-11-10 04:10:40 +0900

    Add doc comments to `PrebuiltLoaderProtocol`

--- Sources/MachOKit/Model/DyldCache/Loader/PrebuiltLoaderProtocol.swift
+++ Sources/MachOKit/Model/DyldCache/Loader/PrebuiltLoaderProtocol.swift
@@ -10,15 +10,32 @@
 import Foundation
 
 public protocol PrebuiltLoaderProtocol {
+    /// Address where this loader is located.
+    ///
+    /// Slides after loading are not included.
     var address: Int { get }
 
+    /// magic of loader starts
     var magic: String? { get }
+    ///  PrebuiltLoader vs JustInTimeLoader
     var isPrebuilt: Bool { get }
     var ref: LoaderRef { get }
 
+    /// path for target mach-o image
+    /// - Parameter cache: DyldCache to which `self` belongs
+    /// - Returns: path name
     func path(in cache: DyldCache) -> String?
+    /// loader reference list of target 's dependencies
+    /// - Parameter cache: DyldCache to which `self` belongs
+    /// - Returns: sequence of loader reference
     func dependentLoaderRefs(in cache: DyldCache) -> DataSequence<LoaderRef>?
 
+    /// path for target mach-o image
+    /// - Parameter cache: DyldCacheLoaded to which `self` belongs
+    /// - Returns: path name
     func path(in cache: DyldCacheLoaded) -> String?
+    /// loader reference list of target 's dependencies
+    /// - Parameter cache: DyldCacheLoaded to which `self` belongs
+    /// - Returns: sequence of loader reference
     func dependentLoaderRefs(in cache: DyldCacheLoaded) -> MemorySequence<LoaderRef>?
 }
