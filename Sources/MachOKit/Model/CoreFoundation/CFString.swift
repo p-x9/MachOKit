b40fa3d37c2b05bf4fcf427b04774a9bc8f7387e
p-x9 <50244599+p-x9@users.noreply.github.com>
2025-02-02 01:12:16 +0900

    Add Unicode and 8-bit detection to CFString protocol

--- Sources/MachOKit/Model/CoreFoundation/CFString.swift
+++ Sources/MachOKit/Model/CoreFoundation/CFString.swift
@@ -12,6 +12,12 @@ import MachOKitC
 public protocol CFStringProtocol {
     var stringOffset: Int { get }
     var stringSize: Int { get }
+
+    var isUnicode: Bool { get }
+    var isEightBit: Bool { get }
+
+    func string(in machO: MachOFile) -> String?
+    func string(in machO: MachOImage) -> String?
 }
 
 public struct CFString64: LayoutWrapper, CFStringProtocol {
@@ -34,6 +40,20 @@ extension CFString64 {
     public var stringSize: Int {
         numericCast(layout._length)
     }
+
+    // ref: https://github.com/apple-oss-distributions/CF/blob/dc54c6bb1c1e5e0b9486c1d26dd5bef110b20bf3/CFString.c#L208C1-L212C3
+    /* !!! Note: Constant CFStrings use the bit patterns:
+    C8 (11001000 = default allocator, not inline, not freed contents; 8-bit; has NULL byte; doesn't have length; is immutable)
+    D0 (11010000 = default allocator, not inline, not freed contents; Unicode; is immutable)
+    The bit usages should not be modified in a way that would effect these bit patterns.
+    */
+    public var isUnicode: Bool {
+        layout._base._cfinfo.0 == 0xD0 // FIXME: consider byte swapped environment (CF_INFO_BITS)
+    }
+
+    public var isEightBit: Bool {
+        layout._base._cfinfo.0 == 0xC8
+    }
 }
 
 extension CFString32 {
@@ -44,24 +64,46 @@ extension CFString32 {
     public var stringSize: Int {
         numericCast(layout._length)
     }
+
+    public var isUnicode: Bool {
+        layout._base._cfinfo.0 == 0xD0
+    }
+
+    public var isEightBit: Bool {
+        layout._base._cfinfo.0 == 0xC8
+    }
 }
 
 extension CFStringProtocol {
     public func string(in machO: MachOFile) -> String? {
         let offset = machO.headerStartOffset + stringOffset
-        return machO.fileHandle.readString(
-            offset: UInt64(offset),
-            size: stringSize
-        )
+        if isUnicode {
+            let data = machO.fileHandle.readData(
+                offset: UInt64(offset),
+                size: numericCast(stringSize) * numericCast(MemoryLayout<UniChar>.size)
+            )
+            return String(bytes: data, encoding: .utf16LittleEndian)
+        } else {
+            return machO.fileHandle.readString(
+                offset: UInt64(offset),
+                size: stringSize
+            )
+        }
     }
 
     public func string(in machO: MachOImage) -> String? {
         guard let ptr = UnsafeRawPointer(bitPattern: UInt(stringOffset)) else {
             return nil
         }
-        return .init(
-            cString: ptr.assumingMemoryBound(to: CChar.self),
-            encoding: .utf8
-        )
+
+        if isUnicode {
+            let data = Data(bytes: ptr, count: numericCast(stringSize) * numericCast(MemoryLayout<UniChar>.size))
+            return String(bytes: data, encoding: .utf16LittleEndian)
+        } else {
+            return .init(
+                cString: ptr.assumingMemoryBound(to: CChar.self),
+                encoding: .ascii
+            )
+        }
     }
 }
