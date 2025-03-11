2499e684e1576bc3ab2bea320aa968cf2149d165
p-x9 <50244599+p-x9@users.noreply.github.com>
2025-03-11 22:08:00 +0900

    Add extension of `_FileIOProtocol`

--- Sources/MachOKit/Extension/FileHandle+.swift
+++ Sources/MachOKit/Extension/FileHandle+.swift
@@ -238,3 +238,236 @@ extension FileHandle {
         )
     }
 }
+
+import FileIO
+
+extension _FileIOProtocol {
+    @_spi(Support)
+    public func readString(
+        offset: UInt64,
+        size: Int
+    ) -> String? {
+        let data = try! readData(
+            offset: numericCast(offset),
+            length: size
+        )
+        return String(cString: data)
+    }
+
+    @_spi(Support)
+    public func readString(
+        offset: UInt64,
+        step: Int = 10
+    ) -> String? {
+        var data = Data()
+        var offset = offset
+        while true {
+            guard let new = try? readData(
+                offset: numericCast(offset),
+                upToCount: step
+            ) else { break }
+            if new.isEmpty { break }
+            data.append(new)
+            if new.contains(0) { break }
+            offset += UInt64(new.count)
+        }
+
+        return String(cString: data)
+    }
+}
+
+extension _FileIOProtocol {
+    func readData(offset: UInt64, size: Int) -> Data {
+        try! readData(offset: numericCast(offset), length: size)
+    }
+
+    func read<T>(offset: UInt64) -> T {
+        try! read(offset: numericCast(offset))
+    }
+}
+
+extension _FileIOProtocol {
+    public func readDataSequence<Element>(
+        offset: UInt64,
+        numberOfElements: Int,
+        swapHandler: ((inout Data) -> Void)? = nil
+    ) /*throws*/ -> DataSequence<Element> where Element: LayoutWrapper {
+        let size = Element.layoutSize * numberOfElements
+        var data = try! readData(
+            offset: numericCast(offset),
+            length: size
+        )
+        precondition(
+            Element.layoutSize == MemoryLayout<Element>.size,
+            "Invalid Layout Size"
+        )
+        precondition(
+            data.count >= size,
+            "Invalid Data Size"
+        )
+        if let swapHandler { swapHandler(&data) }
+        return .init(
+            data: data,
+            numberOfElements: numberOfElements
+        )
+    }
+
+    @_disfavoredOverload
+    public func readDataSequence<Element>(
+        offset: UInt64,
+        numberOfElements: Int,
+        swapHandler: ((inout Data) -> Void)? = nil
+    ) /*throws*/ -> DataSequence<Element> {
+        let size = MemoryLayout<Element>.size * numberOfElements
+        var data = try! readData(
+            offset: numericCast(offset),
+            length: size
+        )
+        precondition(
+            data.count >= size,
+            "Invalid Data Size"
+        )
+        if let swapHandler { swapHandler(&data) }
+        return .init(
+            data: data,
+            numberOfElements: numberOfElements
+        )
+    }
+
+    @_spi(Support)
+    public func readDataSequence<Element>(
+        offset: UInt64,
+        entrySize: Int,
+        numberOfElements: Int,
+        swapHandler: ((inout Data) -> Void)? = nil
+    ) -> DataSequence<Element> where Element: LayoutWrapper {
+        let size = entrySize * numberOfElements
+        var data = readData(
+            offset: offset,
+            size: size
+        )
+        precondition(
+            Element.layoutSize == MemoryLayout<Element>.size,
+            "Invalid Layout Size"
+        )
+        precondition(
+            data.count >= size,
+            "Invalid Data Size"
+        )
+        if let swapHandler { swapHandler(&data) }
+        return .init(
+            data: data,
+            entrySize: entrySize
+        )
+    }
+
+    @_spi(Support)
+    @_disfavoredOverload
+    public func readDataSequence<Element>(
+        offset: UInt64,
+        entrySize: Int,
+        numberOfElements: Int,
+        swapHandler: ((inout Data) -> Void)? = nil
+    ) -> DataSequence<Element> {
+        let size = entrySize * numberOfElements
+        var data = readData(
+            offset: offset,
+            size: size
+        )
+        precondition(
+            data.count >= size,
+            "Invalid Data Size"
+        )
+        if let swapHandler { swapHandler(&data) }
+        return .init(
+            data: data,
+            entrySize: entrySize
+        )
+    }
+}
+
+extension _FileIOProtocol {
+    @_spi(Support)
+    public func read<Element>(
+        offset: UInt64,
+        swapHandler: ((inout Data) -> Void)? = nil
+    ) -> Optional<Element> where Element: LayoutWrapper {
+        var data = readData(
+            offset: offset,
+            size: Element.layoutSize
+        )
+        precondition(
+            Element.layoutSize == MemoryLayout<Element>.size,
+            "Invalid Layout Size"
+        )
+        precondition(
+            data.count >= Element.layoutSize,
+            "Invalid Data Size"
+        )
+        if let swapHandler { swapHandler(&data) }
+        return data.withUnsafeBytes {
+            $0.load(as: Element.self)
+        }
+    }
+
+    @_spi(Support)
+    public func read<Element>(
+        offset: UInt64,
+        swapHandler: ((inout Data) -> Void)? = nil
+    ) -> Optional<Element> {
+        var data = readData(
+            offset: offset,
+            size: MemoryLayout<Element>.size
+        )
+        precondition(
+            data.count >= MemoryLayout<Element>.size,
+            "Invalid Data Size"
+        )
+        if let swapHandler { swapHandler(&data) }
+        return data.withUnsafeBytes {
+            $0.load(as: Element.self)
+        }
+    }
+
+    @_spi(Support)
+    public func read<Element>(
+        offset: UInt64,
+        swapHandler: ((inout Data) -> Void)? = nil
+    ) -> Element where Element: LayoutWrapper {
+        var data = readData(
+            offset: offset,
+            size: Element.layoutSize
+        )
+        precondition(
+            Element.layoutSize == MemoryLayout<Element>.size,
+            "Invalid Layout Size"
+        )
+        precondition(
+            data.count >= Element.layoutSize,
+            "Invalid Data Size"
+        )
+        if let swapHandler { swapHandler(&data) }
+        return data.withUnsafeBytes {
+            $0.load(as: Element.self)
+        }
+    }
+
+    @_spi(Support)
+    public func read<Element>(
+        offset: UInt64,
+        swapHandler: ((inout Data) -> Void)? = nil
+    ) -> Element {
+        var data = readData(
+            offset: offset,
+            size: MemoryLayout<Element>.size
+        )
+        precondition(
+            data.count >= MemoryLayout<Element>.size,
+            "Invalid Data Size"
+        )
+        if let swapHandler { swapHandler(&data) }
+        return data.withUnsafeBytes {
+            $0.load(as: Element.self)
+        }
+    }
+}
