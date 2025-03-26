08287d8caa8d2b7c05e892eed05d49a630953da0
p-x9 <50244599+p-x9@users.noreply.github.com>
2025-03-26 22:57:21 +0900

    Bump `FileIO` to 0.8.0

--- Package.swift
+++ Package.swift
@@ -21,7 +21,7 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(url: "https://github.com/p-x9/swift-fileio.git", from: "0.7.0")
+        .package(url: "https://github.com/p-x9/swift-fileio.git", from: "0.8.0")
     ],
     targets: [
         .target(
