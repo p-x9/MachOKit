86dc6861e3bebeee6ffea52fa863baf2fe58207e
p-x9 <50244599+p-x9@users.noreply.github.com>
2025-06-13 02:10:56 +0900

    dyld_shared_cache_file_path is also available for all Apple platform

--- Sources/MachOKit/DyldCache+Current.swift
+++ Sources/MachOKit/DyldCache+Current.swift
@@ -3,8 +3,12 @@ import MachOKitC
 
 extension DyldCache {
     public var current: DyldCache? {
-        #if os(macOS)
-        return try? DyldCache(url: .init(fileURLWithPath: .init(cString: dyld_shared_cache_file_path())))
+        #if canImport(Darwin)
+        guard let ptr = dyld_shared_cache_file_path() else {
+            return nil
+        }
+        let url: URL = .init(fileURLWithPath: .init(cString: ptr))
+        return try? DyldCache(url: url)
         #else
         return nil
         #endif
