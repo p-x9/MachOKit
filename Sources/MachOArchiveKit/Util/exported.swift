e396296438a544ed213f5e2a0ef2f1119d9c0d68
p-x9 <50244599+p-x9@users.noreply.github.com>
2026-03-24 01:04:00 +0900

    Fix documentation visibility
    - Adjusted documentation visibility for internal imports
    - Reorganized import statements for clarity

--- Sources/MachOArchiveKit/Util/exported.swift
+++ Sources/MachOArchiveKit/Util/exported.swift
@@ -3,8 +3,13 @@
 //  MachOKit
 //
 //  Created by p-x9 on 2026/03/17
-//  
+//
 //
 
-@_exported import MachOKit
-@_exported import ObjectArchiveKit
+@_documentation(visibility: internal)
+@_exported
+import MachOKit
+
+@_documentation(visibility: internal)
+@_exported
+import ObjectArchiveKit
